4a6769041bb7fbaba2065a8883d064d76464edfd
natalialukacova <natalialukacova11@gmail.com>
2024-10-01 19:53:34 +0200

    get/set

--- DudlerMifflin-Solario/Infrastructure/customers.cs
+++ DudlerMifflin-Solario/Infrastructure/customers.cs
@@ -2,5 +2,9 @@
 
 public class customers
 {
-    
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
 }
--- DudlerMifflin-Solario/Infrastructure/properties.cs
+++ DudlerMifflin-Solario/Infrastructure/properties.cs
@@ -2,5 +2,6 @@
 
 public class properties
 {
-    
+    public int id { get; set; }
+    public string PropertyName { get; set; }
 }
